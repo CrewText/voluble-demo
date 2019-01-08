de1094267b2272e4e2956fcd2587d0e88a851c5c
calmcl1 <calmcl1@aol.com>
2019-01-08 19:58:39 +0000

    repackage for heroku

--- src/app/server.js
+++ src/app/server.js
@@ -1,5 +1,16 @@
-var express = require("express");
+//Install express server
+const express = require('express');
+const path = require('path');
 
-var app = express();
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/voluble-dist'));
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/voluble-dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
