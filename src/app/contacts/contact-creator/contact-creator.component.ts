a009b20043def7f5b4a8ea12f451ea13cab80201
calmcl1 <calmcl1@aol.com>
2019-01-28 11:22:51 +0400

    Added contact creator

---
+++ src/app/contacts/contact-creator/contact-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-creator',
+  templateUrl: './contact-creator.component.html',
+  styleUrls: ['./contact-creator.component.css']
+})
+export class ContactCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
